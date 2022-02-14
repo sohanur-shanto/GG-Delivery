cc67e0ec0055fdc1023d9fddc19231157a654738
sohanur-shanto <sohanur.shanto@northsouth.edu>
2022-02-15 00:24:29 +0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-5t(u+!i32g)k-9z(p7ubans%++60lazrckc6&pfmwoaxo7k*-!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
