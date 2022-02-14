4d6ebd4b56341cb8773be5f8f776a4bd1ef0e8e1
sohanur-shanto <sohanur.shanto@northsouth.edu>
2022-02-15 00:23:17 +0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5t(u+!i32g)k-9z(p7ubans%++60lazrckc6&pfmwoaxo7k*-!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
